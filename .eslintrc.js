1534912b70a76188848562d836dbe170f0fcd8b8
Deborah Richins <deborah.richins@gmail.com>
2021-05-12 14:16:42 -0600

    linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,22 @@
 module.exports = {
-  root: true,
-  env: {
-    browser: true,
-    node: true
-  },
-  parserOptions: {
-    parser: 'babel-eslint'
-  },
-  extends: [
-    '@nuxtjs',
-    'plugin:nuxt/recommended'
-  ],
-  plugins: [
-  ],
-  // add your custom rules here
-  rules: {}
+	root: true,
+	env: {
+		browser: true,
+		node: true
+		// es6: true
+	},
+	parserOptions: {
+		parser: 'babel-eslint',
+		ecmaVersion: 2017
+	},
+	extends: [
+		// '@nuxtjs'
+		// 'plugin:nuxt/recommended'
+	],
+	plugins: [
+	],
+	// add your custom rules here
+	rules: {
+		indent: ['warn', 'tab']
+	}
 }
