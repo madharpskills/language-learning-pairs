ebbe3a9d77f8e8ed4bc160a4bda7e4d853250420
Deborah Richins <deborah.richins@gmail.com>
2021-05-13 12:20:14 -0600

    card component fetches from api

--- components/Card.vue
+++ components/Card.vue
@@ -1,6 +1,31 @@
 <template>
-  <div class="Card" style="width:200px;height:100px;border: 1px solid #ffffff;color: #ffffff;"></div>
+  <div class="Card" style="width:200px;height:100px;border: 1px solid #ffffff;color: #ffffff;">
+    <p v-if="$fetchState.pending">Loading....</p>
+    <p v-else-if="$fetchState.error">Error while fetching word</p>
+    <ul v-else>
+      <li>
+        {{ cardstuff.english }}
+      </li>
+      <li>
+        {{ cardstuff.target }}
+      </li>
+    </ul>
+  </div>
 </template>
+
+<script>
+    export default {
+        data() {
+            return {
+                cardstuff: {}
+            }
+        },
+        async fetch() {
+            this.cardstuff = await fetch('http://localhost:3000/api/word/red')
+                .then(res => res.json())
+        }
+    }
+</script>
 
 <style>
 .Card {
